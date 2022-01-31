93b7bebbe392b31bb94edabf0bc3d7e3f079df43
Tangerine Ni <tangerineni@Tangerines-MacBook-Air.local>
2022-01-30 22:11:46 -0800

    refactor

--- yuchen.py
+++ yuchen.py
@@ -1,37 +1,30 @@
+#In minutes
+TOTAL_TRIP_TIME = 45
+WAIT_TIME = 2
+TRAVEL_TIME = 3
+CHARGED_MINS = 4
+#In Cents
+BASE_PRICE = 100
+CHARGED_RATE = 15
 
-school_starts_hour= eval(input("Please enter the hour when school starts: "))
+schoolStartsHour = int(input("Please enter the hour when school starts: "))
+schoolStartsMinute = int(input("Please enter the minute when school starts: "))
+stopNumber = int(input("Please enter your stop number: "))
 
-school_starts_minute= eval(input("Please enter the minute when school starts: "))
+schoolStart = schoolStartsHour * 60 + schoolStartsMinute
 
-school_starts_at_total_minute= school_starts_hour*60+school_starts_minute
+busDepartureTime = schoolStart - TOTAL_TRIP_TIME
+travelTime = (stopNumber - 1) * (TRAVEL_TIME + WAIT_TIME)
+arrive = busDepartureTime + travelTime
+arriveEnd = arrive + WAIT_TIME
 
-print("The school starts time is ", school_starts_hour,":",
-   "school_starts_at_total_minute")
+arriveStr = str(arrive // 60) + ":" + str(arrive % 60).zfill(2)
+arriveEndStr = str(arriveEnd // 60) + ":" + str(arriveEnd % 60).zfill(2)
+print("The bus will be at stop number", stopNumber, "between", arriveStr, "and", arriveEndStr)
 
-stop_number= eval(input("Please enter your stop number: "))
+tripTime = schoolStart - arriveEnd
+print("The length of the trip from stop number", stopNumber, "is", tripTime, "minutes")
 
-TOTAL_TRIP_TIME=45
-bus_departure_time=school_starts_at_total_minute-TOTAL_TRIP_TIME
-
-arrival_time_for_stop={bus_departure_time+(stop_number-1)*(2+3)}-2
-
-departure_time_for_stop=arrival_time_for_stop+2
-
-#stitch the time from minutes to hours
-
-Print("The bus will be at stop number", stop_number,"between",
-      arrival_time_for_stop//60,":","arrival_time_for_stop%60",
-      "and", departure_time_for_stop//60,":","departure_time_for_stop%60")
-
-#The length of the trip from stop to school
-The_length_of_the_trip= {TOTAL_TRIP_TIME-(stop_number-1)*(2+3)}-2
-print("The length of the trip from stop to school is ",
-      The_length_of_the_trip,"minutes")
-
-#The cost of the ticket from stop
-cost_of_ticket=(The_length_of_the_trip)//(stop_number-1)*15+100
-
-#switch the cents to dollars
-print("The cost of the ticket from ",stop_number,'is $',
-      cost_of_ticket//100,","cost_of_ticket%100)
-
+ticketPrice = (tripTime // CHARGED_MINS) * CHARGED_RATE + BASE_PRICE
+ticketPriceStr = "$" + str(ticketPrice // BASE_PRICE) + "." + (str(ticketPrice % 100).zfill(2))
+print("The cost of the ticket from stop number", stopNumber, "is", ticketPriceStr)
